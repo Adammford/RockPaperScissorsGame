f69128217835723c6dd326a29d6dd0312cd69f7e
Adam Ford <ford.adm@gmail.com>
2024-06-18 23:18:51 -0500

    got all buttons working and scores updating. Added beginnings of css styling

--- script.js
+++ script.js
@@ -1,59 +1,51 @@
 
 let playerScore = 0;
 let computerScore = 0;
-const choices = ["paper", "rock", "scissors"];
+const choices = ["Paper", "Rock", "Scissors"];
 
 const ROCK = document.querySelector(".rock");
 const PAPER = document.querySelector(".paper");
 const SCISSORS = document.querySelector(".scissors");
 const PLAYBTN = document.querySelector(".start")
-let PLAYERSCORE = document.querySelector(".playerScore");
-let COMPUTERSCORE = document.querySelector(".computerScore");
+const PLAYERSCORE = document.querySelector(".playerScore");
+const COMPUTERSCORE = document.querySelector(".computerScore");
+const text = document.querySelector(".text");
+
+ROCK.addEventListener("click", () => {playRound('Rock', getComputerChoice());});
+PAPER.addEventListener('click', () => {playRound('Paper', getComputerChoice());});
+SCISSORS.addEventListener('click',() => {playRound('Scissors', getComputerChoice());});
+
 
 function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
 
-ROCK.addEventListener("click", () => {
-   playRound(getHumanChoice('rock'), getComputerChoice());
-    
-});
-PAPER.addEventListener('click', () => {getHumanChoice('paper')});
-SCISSORS.addEventListener('click',() => {getHumanChoice('scissors')});
-
-function getHumanChoice(choice) {
-    console.log(choice);
-    let humanChoice = choice;
-    let isValid = 0;
-
-    while (isValid ==  0) {
-        if (choices.includes(humanChoice)) {
-            isValid = 1;
-            return humanChoice;
-        // } else {
-        //   humanChoice = prompt("Invalid Choice: Please choose Rock, Paper, Scissors: ");
-        };
-    };
-}
-
 function playRound(playerChoice, computerChoice) {
 
     if (playerChoice === computerChoice) {
+        text.textContent = `It's a tie! Both chose ${playerChoice}!`
         console.log(`It's a tie! Both chose ${playerChoice}!`);
     } else if (playerChoice == 'rock' && computerChoice == 'paper') {
+        text.textContent = `Computer wins! ${computerChoice} beats ${playerChoice}!`
         console.log(`Computer wins! ${computerChoice} beats ${playerChoice}!`);
         computerScore++;
     } else if (playerChoice == 'paper' && computerChoice == 'scissors') {
+        text.textContent = `Computer wins! ${computerChoice} beats ${playerChoice}!`
         console.log(`Computer wins! ${computerChoice} beats ${playerChoice}!`);
         computerScore++;
     } else if (playerChoice == 'scissors' && computerChoice == 'rock') {
+        text.textContent = `Computer wins! ${computerChoice} beats ${playerChoice}!`
         console.log(`Computer wins! ${computerChoice} beats ${playerChoice}!`);
         computerScore++;
     } else {
+        text.textContent = `Human wins! ${playerChoice} beats ${computerChoice}!`
         console.log(`Human wins! ${playerChoice} beats ${computerChoice}!`);
         playerScore++;
     }
+
+    PLAYERSCORE.textContent = playerScore;
+    COMPUTERSCORE.textContent = computerScore;
 
 }
 
@@ -68,6 +60,3 @@ function playGame() {
         console.log('Computer wins!')
     }
 }
-
-PLAYERSCORE.textContent = playerScore;
-COMPUTERSCORE.textContent = computerScore;
